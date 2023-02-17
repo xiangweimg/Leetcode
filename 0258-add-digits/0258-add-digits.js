547df3d9946b0f0069ac3e0d5d5ee669ae818cff
Ivy_Liu <106636524+xiangweimg@users.noreply.github.com>
2023-02-17 14:44:45 -0500

    Time: 89 ms (26.63%), Space: 43.6 MB (55.52%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,6 +3,17 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    if (num < 10) return num;
-    return num % 9 === 0 ? 9 : num % 9
+    // if (num < 10) return num;
+    // return num % 9 === 0 ? 9 : num % 9
+    let sum = 0;
+    while(num > 9){
+        let remainder = num % 10
+        sum += remainder
+        num = Math.floor(num/10)
+        if(num < 10){
+            num += sum
+            sum = 0
+        }
+    }
+    return num
 };
