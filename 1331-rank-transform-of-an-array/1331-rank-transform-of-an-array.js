0b29d0ea4b44a4ff94c4ed5e1423584b485c4979
Ivy_Liu <106636524+xiangweimg@users.noreply.github.com>
2023-02-16 23:01:03 -0500

    Time: 394 ms (36.21%), Space: 81.9 MB (5.17%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function(arr) {
+    let copy = [...arr]
+    let sorted = copy.sort((a,b) => a-b)
+    let ranking = {}
+    let res = []
+    let count = 1
+    for(let i = 0; i< sorted.length; i++){
+        let curr = sorted[i]
+        if(!ranking[curr]){
+            ranking[curr] = count
+            count++
+        }
+    }    
+    for(let i = 0; i< arr.length; i++){
+        let curr = arr[i]
+        if(ranking[curr]){
+            res.push(ranking[curr])
+        }
+    } 
+    console.log(ranking)
+    return res
+};
