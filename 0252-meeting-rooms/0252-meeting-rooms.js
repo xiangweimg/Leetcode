7074f11325888bc41944e4dd4c5d591c8e6f86a3
Ivy_Liu <106636524+xiangweimg@users.noreply.github.com>
2023-02-18 10:44:37 -0500

    Time: 82 ms (37.94%), Space: 45.4 MB (7.89%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function(intervals) {
+    let meetings = []
+    let stack = []
+    for(let i = 0; i < intervals.length; i++){
+        let curr = intervals[i]
+        meetings.push({time:curr[0], status:'start', index:i})
+        meetings.push({time:curr[1], status:'end', index:i})
+    }
+    let sorted = meetings.sort((a,b) => {
+        if(a.time === b.time){
+             return a.status.length - b.status.length
+        }else{
+            return a.time - b.time
+        }
+    }) 
+    for(let sort of sorted){
+        let lastI = stack[stack.length - 1]
+        if(sort.index === lastI ){
+            stack.pop()
+        }else if(stack.length > 0){
+            return false
+        }else{
+            stack.push(sort.index)
+        }
+    }
+    return true
+};
