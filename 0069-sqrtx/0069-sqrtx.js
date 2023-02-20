32906745c997f9140e4bd4e87e37be1d93653991
Ivy_Liu <106636524+xiangweimg@users.noreply.github.com>
2023-02-20 17:44:12 -0500

    Time: 213 ms (5.02%), Space: 51.7 MB (5.89%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,9 +3,18 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    let num = 1
-    while(num * num < x){
-        num += 1
+    let left = 0
+    let right = x
+    while(left <= right){
+        console.log(left, right)
+        let mid = Math.floor((left + right) / 2) //4
+        if(mid * mid === x){
+            return mid
+        }else if(mid * mid < x){// 2 * 2
+            left = mid + 1
+        }else if(mid * mid > x){
+           right = mid - 1
+        }
     }
-    return num * num === x ? num : num -1
+    return left -1
 };
