d70b1f3b681fd8a37118a7e63aafd79a95e46d6d
Ivy_Liu <106636524+xiangweimg@users.noreply.github.com>
2023-02-20 20:59:11 -0500

    Time: 87 ms (11.26%), Space: 46.8 MB (5.44%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let left = nums.slice(0,n)
+    let right = nums.slice(n)
+    console.log(left)
+    console.log(right)
+    let result = []
+    for(let i =0; i< n; i++){
+        result.push(left[i])
+        result.push(right[i])
+    }
+    return result
+};
