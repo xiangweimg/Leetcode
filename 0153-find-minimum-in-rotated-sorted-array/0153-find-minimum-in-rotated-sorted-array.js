8f5c45b75fbc46028ba45921f197951e5bb06922
Ivy_Liu <106636524+xiangweimg@users.noreply.github.com>
2023-03-08 11:20:15 -0500

    Time: 58 ms (83.56%), Space: 41.9 MB (62.78%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -3,9 +3,21 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    let min = nums[0]
-    for (let num of nums){
-        if(num < min) return num
+    if(nums.length === 1) return nums[0]
+    // min === undefined ? min = nums[0]: min = min
+    let midpoint = Math.floor(nums.length / 2)
+    let left = nums.slice(0, midpoint);
+    let right = nums.slice(midpoint)
+    let leftEnd = left[left.length -1]
+    let rightEnd = right[right.length -1];
+    if(left[0] < rightEnd) return left[0];
+    // console.log(nums,left,right)
+    if(leftEnd > right[0]){
+        return right[0]
+    }else if(right[0] < rightEnd){
+        return findMin(left)
+    }else{
+        return findMin(right)
     }
-    return min
+    
 };
