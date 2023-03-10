d148a4aac7cb3eea6d4ae856816e405b13cf6175
Ivy_Liu <106636524+xiangweimg@users.noreply.github.com>
2023-03-10 12:49:31 -0500

    Time: 65 ms (72.35%), Space: 44.5 MB (9.04%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let prefix = strs[0]
+    for(let i = 1; i < strs.length; i++){
+	    let index = 0;  
+		let word = ''
+        while(strs[i][index] === prefix[index] && strs[i][index] && prefix[index]){
+            // console.log(strs[i][index], prefix[index])
+            word += strs[i][index]
+            index++
+        }
+        if(!word.length) return word;
+        prefix = word;
+    }
+    return prefix
+};
