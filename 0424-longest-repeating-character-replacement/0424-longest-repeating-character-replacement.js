e686d6d82e688ea23e9643d447dd1b5e080e4fd4
Ivy_Liu <106636524+xiangweimg@users.noreply.github.com>
2023-03-14 17:14:47 -0400

    Time: 72 ms (94.09%), Space: 42.4 MB (72.80%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+  let left = 0;
+  let right = 0; //2
+  let maxCharCount = 0;
+  const visited = {};//window
+
+    while(right < s.length){
+    const char = s[right];
+    visited[char] = visited[char] ? visited[char] + 1 : 1;
+    if(visited[s[right]] > maxCharCount) maxCharCount = visited[s[right]]
+    if(right - left + 1 - maxCharCount > k){
+            visited[s[left]]--
+            left++
+        }
+        right++
+    }
+    return right - left
+  }
