f4b8648ae65fe611d66ab143e999331524f6a5f0
Ivy_Liu <106636524+xiangweimg@users.noreply.github.com>
2023-03-16 20:28:47 -0400

    Time: 63 ms (99.84%), Space: 43.1 MB (89.70%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(str) {//babad
+    let results = "";//bab
+    
+    for(let i = 0; i < str.length; i++) {//2
+        let char = str[i];//bab
+        let left = i;//2
+        let right = i;//2
+        // console.log(left,right)
+        while(left > 0 && str[left - 1] === char) left--;
+        while(right < str.length && str[right + 1] === char) {
+            right++;
+            i++;
+        }
+        while(left >=0 && right < str.length - 1 && str[left - 1] === str[right + 1]) {
+            // console.log(left,right,i)
+            left--;
+            right++;
+            // console.log(left,right,i)
+        }
+        if(results.length < (right - left + 1)) {
+            results = str.slice(left, right + 1);
+        }
+    }
+    return results;
+    
+};
